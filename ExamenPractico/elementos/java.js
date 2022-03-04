f83f56daaeac2a25ea4bdcebe942c5abf79edc99
xime-ngrn <xmorenon2000@alumno.ipn.mx>
2022-03-03 23:38:24 -0600

    archivo no terminado a tiempo

---
+++ ExamenPractico/elementos/java.js
@@ -0,0 +1,6 @@
+function obtenerDatos(){
+    var automovil=document.getElementsById("valor").value;
+    var cuotaI=document.getElementsById("cuota-inicial").value;
+    var periodo=document.getElementsById("periodo ").value;
+
+}
